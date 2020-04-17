d207bf1ea7ba7c7bbe6d7adc70ca9b9a17dd8b07
ben-daghir <bdaghir@gatech.edu>
2020-04-17 16:58:58 -0400

    fixed the data animation

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ var image_b = document.getElementById('image-b');
 var image_k = document.getElementById('image-k');
 var image_labels = document.getElementById('image-labels');
 var image_fids = document.getElementById('image-fids');
+var floating_label = document.getElementById('floating-label');
 
 var translateRGBK = function(scrollPercent) {
     var max_r = -525;
@@ -38,7 +39,12 @@ var fadeDiagrams = function(scrollPercent) {
 var pauseScroll = function(pause) {
     if (pause) {
         scrollContainer.style.overflowY = 'hidden'
-        setTimeout(() => {scrollContainer.style.overflowY = 'scroll'}, 1000)
+        document.body.style.overflowY = 'hidden'
+        setTimeout(() => {
+            scrollContainer.style.overflowY = 'scroll';
+            document.body.style.overflowY = 'scroll'
+        }, 1000)
+
     }
     return false
 }
@@ -48,37 +54,42 @@ var scrollContainer = document.getElementById('scroll-container');
 updateScroll = function(){
     console.log(scrollContainer.scrollTop)
     var scrollTop = scrollContainer.scrollTop
+
     if (500 > scrollTop && scrollTop >= 0) {
-        translateRGBK(scrollTop / 500)
-        textFade(0, 'image-caption')
-        pause1 = true
+        floating_label.style.opacity = 0;
+        translateRGBK(scrollTop / 500);
+        textFade(0, 'image-caption');
+        pause1 = true;
 
     } else if (750 > scrollTop && scrollTop >= 500) {
-        textFade(1, 'image-caption')
-        pause1 = pauseScroll(pause1)
+        floating_label.style.opacity = 0;
+        textFade(1, 'image-caption');
+        pause1 = pauseScroll(pause1);
 
     } else if (1250 > scrollTop && scrollTop >= 750) {
-        translateRGBK(1 - ((scrollTop - 750) / (1250 - 750)))
-        textFade(0, 'image-caption')
-        textFade(0, 'diagram-caption')
-        fadeDiagrams(0)
-        pause2 = true
+        translateRGBK(1 - ((scrollTop - 750) / (1250 - 750)));
+        textFade(0, 'image-caption');
+        textFade(0, 'diagram-caption');
+        fadeDiagrams(0);
+        pause2 = true;
 
     } else if (1500 > scrollTop && scrollTop >= 1250) {
-        translateRGBK(0)
-        textFade(1, 'diagram-caption')
-        fadeDiagrams(1)
-        pause2 = pauseScroll(pause2)
-        translateDiagrams(0)
+        translateRGBK(0);
+        textFade(1, 'diagram-caption');
+        fadeDiagrams(1);
+        pause2 = pauseScroll(pause2);
+        translateDiagrams(0);
 
     } else if (2000 > scrollTop && scrollTop >= 1500) {
-        textFade(0, 'diagram-caption')
-        translateDiagrams((scrollTop - 1500) / (2000 - 1500))
+        textFade(0, 'diagram-caption');
+        translateDiagrams((scrollTop - 1500) / (2000 - 1500));
 
     } else if (2500 > scrollTop && scrollTop >= 2000) {
-        translateDiagrams(1)
+        translateDiagrams(1);
 
     }
+
+    if (25 >= scrollTop) {floating_label.style.opacity = 1;}
 
 }
 
